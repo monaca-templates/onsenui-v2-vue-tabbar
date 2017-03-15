f8cbda402b78ffd0d166a596db5b0299756b8d68
adamkozuch <adam.kozuch@gmail.com>
2017-03-15 14:58:30 +0900

    Add cssnext, update dependencies and App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,12 @@
       <div class="center">{{ title }}</div>
     </v-ons-toolbar>
 
-    <v-ons-tabbar v-ons-index="activeIndex" position="auto" :visible="true">
-      <template slot="pages">
-        <home-page></home-page>
-        <settings-page></settings-page>
-      </template>
-
-      <v-ons-tab v-for="(tab, i) in tabs"
-        :icon="!$platform.isAndroid() && tabs[i].icon"
-        :label="tabs[i].label"
-        :badge="tabs[i].badge"
-      ></v-ons-tab>
+    <v-ons-tabbar position="auto"
+      :tabs="tabs"
+      :visible="true"
+      :index="activeIndex"
+      @update="activeIndex = $event"
+    >
     </v-ons-tabbar>
   </v-ons-page>
 </template>
@@ -22,28 +17,42 @@
 <script>
   import settingsPage from 'Settings';
   import homePage from 'Home';
+  import newsPage from 'News';
 
   export default {
     data() {
       return {
         activeIndex: 0,
         tabs: [
-          {
-            icon: 'ion-home',
-            label: 'Home'
-          },
-          {
-            icon: 'ion-ios-settings',
-            label: 'Settings'
-          }
+	  {
+	    icon: this.md() ? null : 'ion-home',
+	    label: 'Home',
+	    page: homePage
+	  },
+	  {
+	    icon: this.md() ? null : 'ion-ios-bell',
+	    label: 'News',
+	    page: newsPage,
+	    badge: 7
+	  },
+	  {
+	    icon: this.md() ? null : 'ion-ios-settings',
+	    label: 'Settings',
+	    page: settingsPage
+	  }
         ]
       };
+    },
+    methods: {
+      md() {
+        return this.$ons.platform.isAndroid();
+      }
     },
     computed: {
       title() {
         return this.tabs[this.activeIndex].label;
       }
     },
-    components: { homePage, settingsPage }
+    components: { homePage, settingsPage, newsPage }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import VueOnsen from 'vue-onsenui';
 
 // Onsen UI Styling and Icons
-require('onsenui/stylus/blue-basic-theme.styl');
+require('onsenui/css-components-src/src/onsen-css-components.css');
 require('onsenui/css/onsenui.css');
 
 import App from './App.vue';
