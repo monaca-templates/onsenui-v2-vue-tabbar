4f4c4b89cc0df82f684248f9c11e53d9f33de2c1
David Alvarez <davidac0291@gmail.com>
2018-07-09 19:38:28 +0900

    Added Webpack 4 config file plus webpack-serve

---
+++ webpack.config.js
@@ -0,0 +1,153 @@
+const webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+const path = require("path");
+const argvs = require('yargs').argv;
+const devMode = process.env.WEBPACK_SERVE || argvs.mode === 'development';
+
+const DEFAULT_PORT = '8000';
+const host = process.env.MONACA_TERMINAL ? '0.0.0.0': ( argvs.host ? argvs.host : '0.0.0.0' );
+const port = process.env.MONACA_TERMINAL ? argvs.port: ( argvs.port ? argvs.port : DEFAULT_PORT );
+const protocol = process.env.MONACA_TERMINAL ? 'https' : 'http';
+
+let webpackConfig = {
+  cache: devMode ? true: false,
+  entry: ['./src/main.js'],
+
+  output: {
+    path: path.resolve(__dirname, 'www'),
+    filename: 'main.js',
+  },
+
+  optimization: {
+    removeAvailableModules: true,
+    splitChunks: {
+      chunks: 'all'
+    },
+    runtimeChunk: true,
+    removeEmptyChunks: true,
+    mergeDuplicateChunks: true,
+    providedExports: true,
+  },
+  
+  resolve: {
+    extensions: ['.js', '.vue', '.json', '.css', '.html', '.styl'],
+    unsafeCache: devMode ? true: false,
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules'
+    ],
+    alias: {
+      vue:'vue/dist/vue.common.js'
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: [{
+          loader: 'vue-loader'
+        }]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [ 'env' ]
+          } 
+        }]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?\S*)?$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.css$/,
+        use: [          
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }
+    ]
+  },
+  
+  // See below for dev plugin management.
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebPackPlugin({
+      template: 'src/public/index.html.ejs',
+      chunksSortMode: 'dependency'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[name].css"
+    }),
+    new ProgressBarPlugin(),
+  ],
+ 
+  resolveLoader: {
+    modules: [ 'node_modules' ]
+  },
+ 
+  performance: {
+    hints: false
+  }
+};
+
+/*
+* Dev
+*/
+if(devMode) {
+
+  webpackConfig.serve = {
+    port: port,
+    host: host,
+    dev: {
+      publicPath: '/',
+      stats: {
+        colors: true,
+        errorDetails: true,
+        performance: true,
+        source: true,
+        warnings: true,
+        builtAt: true,
+      }
+    },
+    hot: true
+  }
+
+  let devPlugins = [
+    new webpack.LoaderOptionsPlugin({ debug: true })
+  ];
+  
+  webpackConfig.plugins = webpackConfig.plugins.concat( devPlugins　);
+}
+
+module.exports = webpackConfig;
