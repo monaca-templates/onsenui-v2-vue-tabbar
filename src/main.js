a9801cab228c3f615095b524dc70fbd4492c8c53
yong <yong@asial.co.jp>
2021-05-24 17:19:32 +0900

    update onsenui and vueonseui

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import VueOnsen from 'vue-onsenui';
 
 // Onsen UI Styling and Icons
-require('onsenui/css-components-src/src/onsen-css-components.css');
+require('onsenui/css/onsen-css-components.css');
 require('onsenui/css/onsenui.css');
 
 import App from './App.vue';
